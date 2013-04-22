a443a3a7351b8f192f9becbb7134a12f689a2f37
Takashi Ogura <tarchan@gmail.com>
2013-04-22 11:44:17 +0900

    モデル追加

---
+++ app/models/Game.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Game {
+    public int count;
+    public Player[] player;
+    public String result;
+
+	public Game() {
+		count = 4;
+	}
+}
---
+++ app/models/Player.java
@@ -0,0 +1,6 @@
+package models;
+
+public class Player {
+    public int number;
+    public String name;
+}
